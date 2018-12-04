d999be5a0fddaa26a0dbf5151f8cc5a8773fb35b
Kip Landwehr <klandwehr@live.com>
2018-12-04 00:31:27 -0600

    Start or solution for Part 1

---
+++ Day4/Part1.py
@@ -0,0 +1,32 @@
+input = []
+with open('Day4/Input-test.txt', "r") as infile:
+    input = list(map(lambda a: a.strip(), infile.readlines()))
+
+input = map(lambda b: b.split(), input)
+
+# Formats input and parses down to needed info
+for item in input:
+    item[0] = item[0] + " " + item[1]
+    del item[1]
+    if len(item) == 3:
+        del item[2]
+    else:
+        item[1] = item[2][1:]
+        for x in range(3):
+            del item[2]
+
+input.sort(key=lambda c: c[0])
+
+data = []
+
+for item in input:
+    if item[1] ==    
+    
+
+for item in input:
+    print(item)
+#for line in file1:
+#    sections = line.split()
+#    sections[0] = sections[0] + " " + sections[1]
+#    del sections[1]
+#    print(sections)
